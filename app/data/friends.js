ff4b2ee768a62258817f444b49ab96720b9ea509
kavirayani <jkavirayani@gmail.com>
2018-04-17 11:36:02 -0400

    latest commit

---
+++ app/data/friends.js
@@ -0,0 +1,46 @@
+// ===============================================================================
+// DATA
+// Below data will hold all of the friends.
+// ===============================================================================
+
+var friends = [
+    {
+        name: "Ahmed",
+        photo: "https://media.licdn.com/mpr/mpr/shrinknp_200_200/AAEAAQAAAAAAAAq7AAAAJDAwYzI4NTQ4LWYwZWUtNGFkYS1hNTYwLTZjYzkwY2ViZDA3OA.jpg",
+        scores: [
+          "5","1","4","4","5","1","2","5","4","1"
+        ]
+      },
+      {
+        name: "Ahmed",
+        photo: "https://media.licdn.com/mpr/mpr/shrinknp_200_200/AAEAAQAAAAAAAAq7AAAAJDAwYzI4NTQ4LWYwZWUtNGFkYS1hNTYwLTZjYzkwY2ViZDA3OA.jpg",
+        scores: [
+            "5","1","4","4","5","1","2","5","4","1"
+        ]
+      },
+      {
+        name: "Ahmed",
+        photo: "https://media.licdn.com/mpr/mpr/shrinknp_200_200/AAEAAQAAAAAAAAq7AAAAJDAwYzI4NTQ4LWYwZWUtNGFkYS1hNTYwLTZjYzkwY2ViZDA3OA.jpg",
+        scores: [
+            "5","1","4","4","5","1","2","5","4","1"
+        ]
+      },
+      {
+        name: "Ahmed",
+        photo: "https://media.licdn.com/mpr/mpr/shrinknp_200_200/AAEAAQAAAAAAAAq7AAAAJDAwYzI4NTQ4LWYwZWUtNGFkYS1hNTYwLTZjYzkwY2ViZDA3OA.jpg",
+        scores: [
+            "5","1","4","4","5","1","2","5","4","1"
+        ]
+      },
+      {
+        name: "Ahmed",
+        photo: "https://media.licdn.com/mpr/mpr/shrinknp_200_200/AAEAAQAAAAAAAAq7AAAAJDAwYzI4NTQ4LWYwZWUtNGFkYS1hNTYwLTZjYzkwY2ViZDA3OA.jpg",
+        scores: [
+            "5","1","4","4","5","1","2","5","4","1"
+        ]
+      }
+  ];
+  
+  //  we export the array. This makes it accessible to other files using require.
+  module.exports = friends;
+  
